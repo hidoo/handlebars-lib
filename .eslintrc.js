619d41395ee5c071764f01930ef3b099581677fa
hidoo <hidoooo@gmail.com>
2019-09-12 20:57:32 +0900

    chore(eslint): update @hidoo/eslint-config to version 0.2.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,31 @@
 module.exports = {
-  extends: [
-    '@hidoo/eslint-config'
+  'root': true,
+  'extends': [
+    '@hidoo/eslint-config',
+    '@hidoo/eslint-config/+babel'
+  ],
+  'overrides': [
+    // for Mocha
+    {
+      'files': [
+        '**/*.test.js'
+      ],
+      'extends': [
+        '@hidoo/eslint-config/+mocha'
+      ],
+      'rules': {
+        'no-empty-function': 'off',
+        'mocha/no-hooks-for-single-case': 'off'
+      }
+    },
+    // for Node
+    {
+      'files': [
+        '**/src/**/*.js',
+      ],
+      'extends': [
+        '@hidoo/eslint-config/+node'
+      ]
+    }
   ]
-}
+};
